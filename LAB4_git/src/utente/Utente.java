c4a73fb04f8e579636987116af6816d0d7dbb51c
guime <guime@10.179.2.140>
2024-11-29 09:48:45 +0000

    adição dos gets e sets

--- LAB4_git/src/utente/Utente.java
+++ LAB4_git/src/utente/Utente.java
@@ -13,5 +13,36 @@ public class Utente {
 		this.altura = altura;
 		this.peso = peso;
 	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public float getAltura() {
+		return altura;
+	}
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	
 
 }
